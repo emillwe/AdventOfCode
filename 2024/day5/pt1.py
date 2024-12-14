af1165317e5a502a61e19816ecd0a27fec7e78ff
Emmett <emmett.miller@gmail.com>
2024-12-13 21:54:14 -0800

    resolved conflict

--- 2024/day5/pt1.py
+++ 2024/day5/pt1.py
@@ -1,4 +1,4 @@
-isTest = True
+isTest = False
 
 def read_pages(f):
 	result = list()
@@ -30,6 +30,29 @@ def get_order(page_order, seen):
 					s.remove(next)
 	return order
 	
+def is_ordered(l, order):
+	[1, 4, 2, 5, 7]
+	[4, 5, 7]
+	j = 0
+	target = l[j]
+	for elem in order:
+		if elem == target:
+			j += 1
+			if j == len(l):
+				return True
+			target = l[j]
+	return False
+
+def get_middle_page(l):
+	return l[len(l)//2]
+
+def print_data(page_lists, page_order):
+	for l in page_lists:
+		print(l)
+	print()
+	for k, v in page_order.items():
+		print(k, v)
+		
 def main():
 	fn = "input.txt"
 	if isTest:
@@ -38,6 +61,7 @@ def main():
 	page_lists = list()
 	page_order = dict()
 	seen = set()
+	sum_of_middle_pages = 0
 	
 	with open(fn) as f:
 		for line in f:
@@ -54,18 +78,33 @@ def main():
 				page_lists = read_pages(f)
 		f.close()
 		
-		if isTest:
-			print("pages:")
-			for plist in page_lists:
-				print(plist)
-			print("order:")
-			for elem in page_order:
-				print(elem, page_order[elem])
-				
+#		if isTest:
+#			print("pages:")
+#			for plist in page_lists:
+#				print(plist)
+#			print("order:")
+#			for elem in page_order:
+#				print(elem, page_order[elem])
+		
+		print("intersection:",len(set.intersection(seen, page_order.keys())))
+		print("equal sets:", seen == page_order.keys())
+		
+		print_data(page_lists, page_order)
+		
 		# get order
 		order = get_order(page_order, seen)
+		order.reverse()
+#		print("order:", order)
+		
 		
 		# check for valid orders
-		
+		for l in page_lists:
+			if is_ordered(l, order):
+#				print("ordered!")
+				sum_of_middle_pages += get_middle_page(l)
+#			else:
+#				print("not ordered:", l)
+				
+#		print(f"sum: {sum_of_middle_pages}")
 if __name__ == "__main__":
     main()
